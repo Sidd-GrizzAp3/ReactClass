362bc822285912717fa884d5d071eb700ea4c2fb
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2017-01-21 22:37:49 -0500

    added new routing: cancel button and submit using CONTEXT

--- Project3/src/components/posts_new.js
+++ Project3/src/components/posts_new.js
@@ -1,21 +1,35 @@
-import React, {Component}   from 'react'; 
-import {bindActionCreators} from 'redux'; 
-import {connect}            from 'react-redux'; 
-import {reduxForm}          from 'redux-form';
-import {createNewPost}      from '../actions/index'; 
+import React, { Component, PropTypes }   from 'react'; 
+import { bindActionCreators } from 'redux'; 
+import { connect }            from 'react-redux'; 
+import { reduxForm }          from 'redux-form';
+import { createNewPost }      from '../actions/index'; 
+import { Link }             from 'react-router'; 
 
 class PostsNew extends Component {
     
-   
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+
+    onSubmit(props) {
+            this.props.createNewPost(props) // once you get the promise resolved you actually get a resovled proise returned! 
+            .then( () => {
+                // blog post has been creaed, navitage the user to the index 
+                // we navitage by calling this.context.router.push with the 
+                // new path of the destination route 
+                this.context.router.push('/'); 
+            });                      
+    }; 
     render () {
          const { fields: { title, categories, content } , handleSubmit} = this.props; 
          // same thing as const handSubmit = this.props.handleSubmit
-    
+        
         return(
-            <form onSubmit={handleSubmit(this.props.createNewPost)}>
+            <form onSubmit= {handleSubmit( this.onSubmit.bind(this) ) }>
                 <h3>Create A New Post </h3>
                 
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger': '' }`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />   
                     <div className="text-help"> 
@@ -23,17 +37,24 @@ class PostsNew extends Component {
                     </div>             
                 </div>
                 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger': '' }`}>
                  <label>Categories</label>
-                 <input type="text" className="form-control" {...categories} />                
+                 <input type="text" className="form-control" {...categories} /> 
+                 <div className="text-help"> 
+                        {categories.touched ? categories.error : null }
+                    </div>                  
                 </div>
                 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger': '' }`}>
                  <label>Content</label>
-                 <textarea type="text" className="form-control" {...content} />                
+                 <textarea type="text" className="form-control" {...content} /> 
+                 <div className="text-help"> 
+                        {content.touched ? title.error : null }
+                    </div>                  
                 </div>
                 
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to='/' className='btn btn-danger'> Cancel </Link> 
             </form>
         ); 
     }
@@ -46,6 +67,13 @@ function validate (values) {
     if(!values.title) {
         errors.title = 'Enter a title';
     }
+    if (!values.categories) {
+        errors.categories = 'Enter a category'; 
+    }   
+    if (!values.content) {
+        errors.content = 'Enter some content'; 
+    }
+
     return errors; 
 }
 
@@ -54,7 +82,7 @@ function validate (values) {
 export default reduxForm({
     form:   'PostsNewForm', 
     fields: ['title', 'categories', 'content'], 
-    validate
+    validate  
     },
     null, 
     {createNewPost} // this it he action creator so yo'ur emapping this funciton's dispatch and props.  
