4e6504c8c1dc32a6d060e8c2dd0679c7ea67b689
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2017-02-18 15:12:25 -0500

    adding Testing folder for advanced react class

--- Project4_ReduxThunk/Basics_Of_Redux_Thunk/src/actions/index.js
+++ Project4_ReduxThunk/Basics_Of_Redux_Thunk/src/actions/index.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 export function fetchUsers() {
   const request = axios.get('http://jsonplaceholder.typicode.com/users');
 
+  // we can return a plain javascript function 
+  // via redux-thunk, instaed of a plain javascript object. 
   return (dispatch) => {
     request.then(({data}) => {
-      dispatch({ type: 'FETCH_PROFILES', payload: data })
+      dispatch({type: 'FETCH_PROFILES', payload: data});
     });
   };
 }
