8b5159cfaa66b7e9c69192d48e22a58412a5011d
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2017-02-18 15:24:06 -0500

    started the testing videos, nothing awesome just, just set up the testing framkework using mocha

--- AdvancedReactClass/Testing/test/components/app_test.js
+++ AdvancedReactClass/Testing/test/components/app_test.js
@@ -1,14 +1,16 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+// import two helpers from the test helpers 
+import { renderComponent, expect } from '../test_helper'; 
+// import the app  the unit under test
+import App from '../../src/components/app'; 
 
-describe('App' , () => {
-  let component;
+//testing keywoards 
+// use decribe to group together similar tests 
+describe('App')
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+// use 'it' to test a signle attribute of a target 
+//  here is exactly what we're testing using 'it' 
+it('shows the correct text') 
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+
+
+//expect 
