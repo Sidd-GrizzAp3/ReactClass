82b05b74504225c7da1e5b3626fb380e41af8baa
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2017-02-26 15:34:55 -0500

    added multiple testing and nested test cases

--- AdvancedReactClass/Testing/test/components/app_test.js
+++ AdvancedReactClass/Testing/test/components/app_test.js
@@ -5,13 +5,21 @@ import App from '../../src/components/app';
 
 //testing keywoards 
 // use decribe to group together similar tests 
-describe('App')
+describe('App', () => {
+    let component; 
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
+    // use 'it' to test a signle attribute of a target 
+    //  here is exactly what we're testing using 'it' 
+    //  make sure this is very easy to understand. 
+    it('shows a comment box', () => {
+        // making sure that there is a child 
+        // exists 
+        expect(component.find('.comment-box')).to.exist;
+    });
+});
 
-// use 'it' to test a signle attribute of a target 
-//  here is exactly what we're testing using 'it' 
-it('shows the correct text') 
 
 
 
-//use 'exepct' to make an 'assertion' about a target (device under test ) 
-expect 
--- AdvancedReactClass/Testing/test/test_helper.js
+++ AdvancedReactClass/Testing/test/test_helper.js
@@ -26,6 +26,7 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
+
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
