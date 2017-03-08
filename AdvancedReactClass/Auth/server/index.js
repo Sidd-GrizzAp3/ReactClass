0aac7dee8c61e34f64cb3bdef4a793068a22b862
Sidd-GrizzAp3 <its.sidd.artha@gmail.com>
2017-03-08 08:15:06 -0500

    started server code / app setup

--- AdvancedReactClass/Auth/server/index.js
+++ AdvancedReactClass/Auth/server/index.js
@@ -1,2 +1,20 @@
 // main starting point of application 
+const express       = require('express'); 
+const http          = require('http');
+const bodyParser    = require('body-parser');
+const morgan        = require ('morgan');
+const app           = express(); 
+const router        = require('./router');
 
+// App set up 
+// middlewear setup 
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'})); // parses requests as only json formatted 
+
+router(app);
+
+// Server Setup 
+const port = process.env.PORT || 3000; 
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server Listening on Port:' + port);
